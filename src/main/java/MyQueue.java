c1a819cb5115c0f78931a88fa2c4bc8cac4abda7
Hristina29 <150126492+Hristina79@users.noreply.github.com>
2025-04-10 11:38:43 +0200

    create an interface for Queue

---
+++ src/main/java/MyQueue.java
@@ -0,0 +1,9 @@
+package src.main.java;
+
+public interface MyQueue<T> {
+    void enqueue(T item);  // Add to the back
+    T dequeue();           // Remove from the front
+    T peek();              // Check front element
+    boolean isEmpty();
+    int size();
+}
