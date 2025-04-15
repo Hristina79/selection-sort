4355b00f0e90e8737bf7518e5ad4b809fd84eb73
Hristina Pavlova <150126492+Hristina79@users.noreply.github.com>
2025-04-15 13:00:10 +0200

    Update SelectionSort.java

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -6,7 +6,7 @@ public class SelectionSort<T extends Comparable<T>> {
 
         // check if input is from a compatible type (of SingleLinkedList)
         if (!(queue instanceof SingleLinkedList)) {
-            throw new IllegalArgumentException("This sorter only works with SingleLinkedListQueue!");
+            throw new IllegalArgumentException("This sorter only works with SingleLinkedList!");
         }
 
         // Cast SingleLinkedList for direct access
